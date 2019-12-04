326312dd24ab5385bc2734d38a801720d5c31abf
tutkuince <tutku.ince@outlook.com>
2019-12-04 14:15:21 +0300

    retrieveDogBreeds, retrieveDogBreedById and retrieveDogNames methods created with their own queries

--- src/main/java/com/muditasoft/springbootrestfullapis/repository/DogRepository.java
+++ src/main/java/com/muditasoft/springbootrestfullapis/repository/DogRepository.java
@@ -2,6 +2,17 @@ package com.muditasoft.springbootrestfullapis.repository;
 
 import com.muditasoft.springbootrestfullapis.entity.Dog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DogRepository extends JpaRepository<Dog, Long> {
+    @Query("select d.id, d.breed from Dog d")
+    List<String> retrieveDogBreeds();
+
+    @Query("select d.id, d.breed from Dog d where d.id=:id")
+    String retrieveDogBreedById(Long id);
+
+    @Query("select d.id, d.name from Dog d")
+    List<String> retrieveDogNames();
 }
