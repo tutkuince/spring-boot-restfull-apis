962460000906f48680a0e85c163806b0ca92ca81
tutkuince <tutku.ince@outlook.com>
2019-12-04 13:25:19 +0300

    Created LocationController and injected LocationService and invoke retrieveLocations() in getAllLocations

---
+++ src/main/java/com/muditasoft/springbootrestfullapis/web/LocationController.java
@@ -0,0 +1,25 @@
+package com.muditasoft.springbootrestfullapis.web;
+
+import com.muditasoft.springbootrestfullapis.entity.Location;
+import com.muditasoft.springbootrestfullapis.service.LocationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class LocationController {
+
+    private final LocationService locationService;
+
+    public LocationController(LocationService locationService) {
+        this.locationService = locationService;
+    }
+
+    @GetMapping("/location")
+    public ResponseEntity<List<Location>> getAllLocations() {
+        List<Location> locationList = locationService.retrieveLocations();
+        return ResponseEntity.ok(locationList);
+    }
+}
